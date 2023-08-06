5d9bae8a0945fdd64d0ec8c88cd00c9677d38519
Rahul Lahria <rahul.lahoria@yahoo.com>
2023-08-06 07:44:05 +0530

    portfolio web

--- frontend/src/screens/Doubt/Doubt.js
+++ frontend/src/screens/Doubt/Doubt.js
@@ -2,7 +2,7 @@ import Gun from 'gun'
 import {useEffect, useState} from 'react'
 
 const gun = Gun({
-  peers: ['http:216.48.177.247:8000/gun'] // Put the relay node that you want here
+  peers: ['https:img-transformer-v2.mogiio.com/gun'] // Put the relay node that you want here
   // peers: ['https:dev-apis.mogiio.com/gc-lms-bc/gun']
 })
 
